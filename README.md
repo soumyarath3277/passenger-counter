3175b53a39d20d3f978d570efb72054d3ffd617d
soumyarath3277 <78521535+soumyarath3277@users.noreply.github.com>
2022-08-24 12:55:12 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+This website is made using html, css and javascript.
+It is used to count the no of passengers in a metro and save it.
---
+++ index.js
@@ -0,0 +1,19 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    if(count>=9){
+        count==0;
+    }
+
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+
+}
+
